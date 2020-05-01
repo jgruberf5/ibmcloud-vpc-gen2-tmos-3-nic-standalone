a7c55e75823028b98ede4596505d9cd30e4dffb2
f5craigsca <c.scarborough@f5.com>
2020-05-01 12:28:25 -0400

    1st working version

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/user_data.yaml")}"
   vars = {
     tmos_admin_password = var.tmos_admin_password
-    license_basekey     = var.tmos_license_basekey
+    tmos_license_basekey = var.tmos_license_basekey
   }
 }
 
@@ -190,7 +190,7 @@ resource "ibm_is_security_group_rule" "f5_tmm_out_icmp" {
 resource "ibm_is_instance" "f5_ve_instance" {
   name    = var.instance_name
   image   = data.ibm_is_image.tmos_image.id
-  profile = data.ibm_is_instance_profile.instance_profile
+  profile = data.ibm_is_instance_profile.instance_profile.id
   primary_network_interface {
     name            = "management"
     subnet          = data.ibm_is_subnet.f5_management.id
@@ -208,7 +208,7 @@ resource "ibm_is_instance" "f5_ve_instance" {
   }
   vpc  = data.ibm_is_subnet.f5_management.vpc
   zone = data.ibm_is_subnet.f5_management.zone
-  keys      = [data.ibm_is_ssh_key.f5_ssh_pub_key.id]
+  keys = [data.ibm_is_ssh_key.f5_ssh_pub_key.id]
   user_data = data.template_file.user_data.rendered
 }
 
@@ -218,7 +218,8 @@ resource "ibm_is_floating_ip" "f5_management_floating_ip" {
   target = ibm_is_instance.f5_ve_instance.primary_network_interface.0.id
 }
 
-resource "ibm_is_floating_ip" "f5_external_floating_ip" {
-  name   = "external-floating-ip"
-  target = ibm_is_instance.f5_ve_instance.network_interfaces.1.id
-}
+#resource "ibm_is_floating_ip" "f5_external_floating_ip" {
+#  name   = "external-floating-ip"
+#  target = ibm_is_instance.f5_ve_instance.network_interfaces.1.id
+#  depends_on = [ibm_is_instance.f5_ve_instance, ibm_is_floating_ip.f5_management_floating_ip]
+#}
